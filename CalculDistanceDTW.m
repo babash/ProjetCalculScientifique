01c2b6fee4977891a5f9728d9b377db8c3213f8e
Naowak <yannisbendi@hotmail.Fr>
2016-10-17 16:44:14 +0200

    ajout premier commentaire

--- CalculDistanceDTW.m
+++ CalculDistanceDTW.m
@@ -1,9 +1,23 @@
 function [D,g] = CalculDistanceDTW(sequence1, sequence2, distance, contrainte = +inf)
+    %On prepare la taille de notre tableau a deux dimensions. 
+    %Etant donné que nous devons rajouter une ligne et une colonne pour faire nos calculs,
+    %Les dimensions de notre tableau sont donc la longueur de nos de deux matrices +1.
     [hauteur, largeur] = size(sequence1);
     size_1 = largeur + 1;
     [hauteur, largeur] = size(sequence2);
     size_2 = largeur + 1;
+    %Creation du tableau g
     g = zeros(size_1, size_2);
+    
+    %Pour pouvoir stocker pour chaque case le chemin parcouru,
+    %nous créons deux tableaux (un pour l'abscisse, l'autre pour l'ordonné).
+    %Ces deux tableaux permettent de retrouver l'indice de la case précédente.
+    %Ils sont modélisés par un tableau à trois dimensions.
+    %Les deux premières dimensions sont les dimensions du tableau sur lequel on travail.
+    %La troisième (et dernière) dimension permet la gestion des deux tableaux.
+    %Celle-ci est donc de taille 2.
+    %Ainsi, si l'on souhaite connaitre les coordonnées de la case (x, y) de notre tableau g :
+    %Il faut regarder les cases de coordonnées (x, y,  1) pour l'ordonnée et (x, y, 2) pour l'abscisse
     case_prec = zeros(size_1, size_2, 2);
     for i = 2 : size_1
         g(i, 1) = +inf;
@@ -43,5 +57,8 @@ function [D,g] = CalculDistanceDTW(sequence1, sequence2, distance, contrainte = 
     endfor
     D = g(size_1, size_2)/(size_1+size_2);
     %imagesc(g);
+    %D
+    g
+    case_prec
             
 endfunction
